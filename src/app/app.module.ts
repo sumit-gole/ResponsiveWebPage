990cb075245a91904517181ede354cebf343d3cf
Sumit Gole <67162604+sumit-gole@users.noreply.github.com>
2021-12-13 18:51:42 +0530

    Final Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    OffersComponent,
+    GalleryComponent,
+    ServicesComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
